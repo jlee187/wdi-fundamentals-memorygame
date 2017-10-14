a762fadfc0e800a6ed6a0024ab6548cdcc4818e7
Jonathan Lee <jonathanlee@Jonathans-MacBook-Pro.local>
2017-10-14 02:45:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,46 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+card:"queen",
+suit:"hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{card:"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+{card:"king",
+suit:'hearts',
+cardImage:"images/queen-of-diamonds.png"
+},
+{card : "king",
+suit :'diamonds',
+cardImage:"images/queen-of-diamonds.png"
+}
+];
+
 var cardsInPlay =[];
 
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+alert("You found a match!");
 } else {
-console.log("Sorry, try again.");
+alert("Sorry, try again.");
 }
 };
 
 var flipCard = function(cardId){
-	
-	console.log("User flipped "+cards[cardId]);
 
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped "+cards[cardId].card);
+
+	cardsInPlay.push(cards[cardId].card);
 
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	}
-	
+	console.log(cards[cardId].cardImage);
+
+	console.log(cards[cardId].suit);
+
 };
 
 
